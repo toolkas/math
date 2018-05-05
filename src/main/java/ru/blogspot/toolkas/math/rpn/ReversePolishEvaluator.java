5846297613502529cae738b5902333a84148c980
tulkas <1@id-mt.ru>
2018-05-05 14:19:29 +0300

    стек хранит Double

--- src/main/java/ru/blogspot/toolkas/math/rpn/ReversePolishEvaluator.java
+++ src/main/java/ru/blogspot/toolkas/math/rpn/ReversePolishEvaluator.java
@@ -13,50 +13,76 @@ public class ReversePolishEvaluator {
     private static final String MULTIPLY = "*";
 
     public double eval(String expression) {
+        //разбиваем строку пробелами
         String[] values = Objects.requireNonNull(expression).split(" ");
 
-        Stack<String> stack = new Stack<>();
+        Stack<Double> stack = new Stack<>();
         for (String value : values) {
             if (StringUtils.isBlank(value)) {
+                //если встретился пробел - пропускаем и идем дальше
                 continue;
             }
 
             if (NumberUtils.isNumber(value)) {
-                stack.push(value);
+                //встретилось число - переносим в стек
+                stack.push(Double.parseDouble(value));
             } else if (PLUS.equals(value)) {
-                String val2 = stack.pop();
-                String val1 = stack.pop();
+                //Встретился знак сложения.
+                //Читаем последнее число
+                double val2 = stack.pop();
+                //Читаем предпоследнее число
+                double val1 = stack.pop();
 
-                double result = Double.parseDouble(val1) + Double.parseDouble(val2);
-                stack.push(String.valueOf(result));
+                //Складываем числа
+                double result = val1 + val2;
+                //Результат сложения переносим в стек
+                stack.push(result);
             } else if (MINUS.equals(value)) {
-                String val2 = stack.pop();
-                String val1 = stack.pop();
+                //Встретился знак вычитания.
+                //Читаем последнее число
+                double val2 = stack.pop();
+                //Читаем предпоследнее число
+                double val1 = stack.pop();
 
-                double result = Double.parseDouble(val1) - Double.parseDouble(val2);
-                stack.push(String.valueOf(result));
+                //Из предпоследнего числа вычитаем последнее
+                double result = val1 - val2;
+                //Результат вычитания переносим в стек
+                stack.push(result);
             } else if (DIVIDE.equals(value)) {
-                String val2 = stack.pop();
-                String val1 = stack.pop();
+                //Встретился знак деления.
+                //Читаем последнее число
+                double val2 = stack.pop();
+                //Читаем предпоследнее число
+                double val1 = stack.pop();
 
-                double result = Double.parseDouble(val1) / Double.parseDouble(val2);
-                stack.push(String.valueOf(result));
+                //Делим предпоследнее число на последнее
+                double result = val1 / val2;
+                //Результат деления переносим в стек
+                stack.push(result);
             } else if (MULTIPLY.equals(value)) {
-                String val2 = stack.pop();
-                String val1 = stack.pop();
+                //Встретился знак умножения.
+                //Читаем последнее число
+                double val2 = stack.pop();
+                //Читаем предпоследнее число
+                double val1 = stack.pop();
 
-                double result = Double.parseDouble(val1) * Double.parseDouble(val2);
-                stack.push(String.valueOf(result));
+                //Перемножаем числа
+                double result = val1 * val2;
+                //Результат перемножения переносим в стек
+                stack.push(result);
             } else {
                 throw new IllegalArgumentException("unsupported value: " + value);
             }
         }
 
-        String result = stack.pop();
-        if(!stack.isEmpty()) {
+        //Читаем результат вычисления из стека
+        double result = stack.pop();
+
+        //Если в стеке еще что-то осталось после вычислений - значит исходное выражение было некорректно. Ошибка
+        if (!stack.isEmpty()) {
             throw new IllegalArgumentException("expression[" + expression + "] is incorrect");
         }
 
-        return Double.parseDouble(result);
+        return result;
     }
 }
